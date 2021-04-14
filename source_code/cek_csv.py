aff7d16a94fc327d35e6ab15cf9359d00a8270bf
Eraraya Morenzo <morenzomuten@gmail.com>
2021-04-14 23:05:51 +0700

    Moved error handling to kantongajaib.py

--- source_code/cek_csv.py
+++ source_code/cek_csv.py
@@ -2,32 +2,17 @@ import sys
 import os 
 from constant import nama_csv
 
-def folder_tidak_ada():
-	sys.exit("Folder tidak ada!")
-
-def csv_tidak_ada():
-	pesan_error = '''
-File CSV tidak sesuai!
-File CSV yang diperlukan:
-	user,
-	gadget,
-	consumable,
-	consumable_history,
-	gadget_borrow_history,
-	gadget_return_history
-'''
-	sys.exit(pesan_error)
-
-def cek_argumen(argument):
-	jumlah_argumen = len(argument)
+def cek_argumen(argumen):
+	jumlah_argumen = len(argumen)
 	if jumlah_argumen > 2:
-		print("Argumen yang diberikan terlalu banyak!")
-		print(" Usage: \x1B[3mpython kantongajaib.py <nama_folder>\x1B[23m")
+		nama_folder_tujuan = " ".join(argumen[1:jumlah_argumen])
+		return nama_folder_tujuan
 	elif jumlah_argumen < 2:
 		print("Tidak ada nama folder yang diberikan!")
 		print(" Usage: \x1B[3mpython kantongajaib.py <nama_folder>\x1B[23m")
+		sys.exit()
 	elif jumlah_argumen == 2:
-		nama_folder_tujuan = argument[1]
+		nama_folder_tujuan = argumen[1]
 		return nama_folder_tujuan
 
 def cek_folder(nama_folder_tujuan):
@@ -40,17 +25,19 @@ def cek_folder(nama_folder_tujuan):
 				folder_tujuan_dir = os.path.join(root,nama_folder_tujuan)			
 				break
 		if folder_found:
-			return folder_tujuan_dir
+			return folder_tujuan_dir, workspace
 			break
 		elif not folder_found:
-			folder_tidak_ada()
+			return 'folder tidak ada', workspace
 
 def cek_csv(argumen):
 	nama_folder_tujuan = cek_argumen(argumen)
-	path = cek_folder(nama_folder_tujuan)
-	files = os.listdir(path)
+	folder_tujuan_dir, workspace = cek_folder(nama_folder_tujuan)
+	if folder_tujuan_dir == 'folder tidak ada':
+		return 'folder tidak ada'
+	files = os.listdir(folder_tujuan_dir)
 	csv_found = all(item in files for item in nama_csv)
 	if csv_found:
-		return path
+		return folder_tujuan_dir
 	elif not csv_found:
-		csv_tidak_ada()
+		return 'csv tidak ada'
