34c4fb0c4757852c6ec472bcf0a3e69953ece904
Eraraya Morenzo <morenzomuten@gmail.com>
2021-04-14 23:08:26 +0700

    Cleaned up.

--- source_code/save.py
+++ source_code/save.py
@@ -1,43 +1,6 @@
-from cek_csv import cek_csv
-
-# database id
-user = 0
-gadget = 1
-consumable = 2
-consumable_history = 3
-gadget_borrow_history = 4
-gadget_return_history = 5
-
-databases = [
-			 #user
-			 [
-			  ['id', 'username', 'nama', 'alamat', 'password', 'role'], 
-			  [1, 'morenzoe', 'Eraraya Morenzo Muten', 'Depok', 'kucing123', 'Admin'], 
-			  [2, 'nisa,Khafifanisa', 'Bukittinggi', '123sti', 'User'], 
-			  [3, 'raihan', 'M. Raihan Aulia', 'Indonesia,kucing456,User'], 
-			  [4, 'abik', 'Atabik Azfa', 'Rumah', 'inipass', 'User']
-			 ], 
-			 #gadget
-			 [
-			  ['id', 'nama', 'deskripsi', 'jumlah', 'rarity', 'tahun_ditemukan']
-			 ], 
-			 #consumable
-			 [
-			  ['id', 'nama', 'deskripsi', 'jumlah', 'rarity']
-			 ], 
-			 #consumable_history
-			 [
-			  ['id', 'id_pengambil', 'id_consumable', 'tanggal_peminjaman', 'jumlah']
-			 ], 
-			 #gadget_borrow_history
-			 [
-			  ['id', 'id_peminjam', 'id_gadget', 'tanggal_peminjaman', 'jumlah']
-			 ], 
-			 #gadget_return_history
-			 [
-			  ['id', 'id_peminjam', 'id_gadget', 'tanggal_pengembalian']
-			 ]
-			]
+import os
+from constant import *
+from cek_csv import cek_folder
 
 def to_str(array_word_int):
 # menghasilkan word dalam type str
@@ -55,24 +18,46 @@ def array_word_to_row(array_word):
 	row = ";".join(array_word) + "\n"
 	return row
 
-def array_to_csv(path, array):
-	csv = []
+def array_to_csv(csv, folder_path, database):
+	save_csv = []
 	for array_word_int in database:
 		array_word = to_str(array_word_int)
 		row = ";".join(array_word) + "\n"
-		csv.append(row)
-	return csv
-	
-	csv_path = os.path.join(path,csv)
+		save_csv.append(row)
+	csv_path = os.path.join(folder_path,csv)
 	f = open(csv_path,"w")
-	f.write(array)
+	for row in save_csv:
+		f.write(row)
 	f.close()
 
-def save(databases,nama_csv):
+def save(databases):
 	folder_save = input("Masukkan nama folder penyimpanan: ")
-	#tulis csv
+	# cek foldernya ada dan csvnya ada atau ngga
+	folder_path, workspace = cek_folder(folder_save)
+	# kalo gaada folder , buat folder baru
+	if folder_path == 'folder tidak ada':
+		folder_path = os.path.join(workspace, folder_save)
+		# buat folder, buat csv
+		os.makedirs(folder_path)
+	# kalo folder ada, overwrite
 	print("Saving...")
-	
-	for database in databases:
-		array_to_csv(database)
-	
+	for i in range(len(databases)):
+		csv = nama_csv[i]
+		array_to_csv(csv, folder_path, databases[i])
+	print("Data telah disimpan pada folder", folder_save + "!")
+
+'''
+hasil array to csv
+['id;username;nama;alamat;password;role\n', 
+'1;morenzoe;Eraraya Morenzo Muten;Depok;kucing123;Admin\n', 
+'2;nisa,Khafifanisa;Bukittinggi;123sti;User\n', 
+'3;raihan;M. Raihan Aulia;Indonesia,kucing456,User\n', 
+'4;abik;Atabik Azfa;Rumah;inipass;User\n']
+
+goal
+['id;username;nama;alamat;password;role\n', 
+'1;morenzoe;Eraraya Morenzo Muten;Depok;kucing123 ;Admin\n', 
+'2;nisa,Khafifanisa;Bukittinggi;123sti;User\n', 
+'3;raihan;M. Raihan Aulia;Indonesia,kucing456,User\n', 
+'4;abik;Atabik Azfa; Rumah;inipass;User']
+'''
