c849c9523b3f92b1b0033e195bd2ee4ea814cc16
Eraraya Morenzo <morenzomuten@gmail.com>
2021-04-14 20:14:11 +0700

    Separated dependencies import to functions.py

--- source_code/constant.py
+++ source_code/constant.py
@@ -1,20 +1,3 @@
-# import fungsi
-from carirarity import carirarity
-from caritahu import caritahu
-from hapusitem import hapusitem
-from help import help
-from kembalikan import kembalikan
-from login import login
-from minta import minta
-from pinjam import pinjam
-#from register import register # register belum mengubah list
-from riwayatpinjam import riwayatpinjam
-from riwayatkembali import riwayatkembali
-from riwayatambil import riwayatambil
-from save import save
-from tambahitem import tambahitem
-from ubahjumlah import ubahjumlah
-
 # database id
 user = 0
 gadget = 1
@@ -30,21 +13,3 @@ nama_csv = ["user.csv",
 		"consumable_history.csv",
 		"gadget_borrow_history.csv",
 		"gadget_return_history.csv"]
-		
-# nama-nama fungsi
-dict_program = {'login' : login,
-				'carirarity' : carirarity,
-				'caritahu' : caritahu,
-				'tambahitem' : tambahitem,
-				'hapusitem' : hapusitem,
-				'ubahjumlah' : ubahjumlah,
-				'pinjam' : pinjam,
-				'kembalikan' : kembalikan,
-				'minta' : minta,
-				'riwayatpinjam' : riwayatpinjam,
-				'riwayatkembali' : riwayatkembali,
-				'riwayatambil' : riwayatambil,
-				'save' : save,
-				'help' : help,
-				'exit' : exit
-				}
