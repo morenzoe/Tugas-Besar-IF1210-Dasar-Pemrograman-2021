bf18ea4afc63d7d8adc4107b809c35e279147490
Eraraya Morenzo <morenzomuten@gmail.com>
2021-04-14 23:34:53 +0700

    Added function return, removed extra tabs.

--- source_code/login.py
+++ source_code/login.py
@@ -1,11 +1,12 @@
 # prosedur akhir
-def login(user):
-        uname = input("Ketik username : ")
-        passw = input("Ketik password : ")
+def login(databases):
+	uname = input("Ketik username : ")
+	passw = input("Ketik password : ")
 
-        #..... belum nemu code nya
-        
-        if uname != usernama or passw != password :
-                print("Login gagal. username atau password salah")
-        else:
-                print("Login berhasil")
+	#..... belum nemu code nya
+	
+	if uname != usernama or passw != password :
+			print("Login gagal. username atau password salah")
+	else:
+			print("Login berhasil")
+	return databases
