ca0162c028316082e8b5fe8a823cbaf5cf97bd81
FlitzyBlue332 <raihanaulia332@gmail.com>
2021-04-15 10:01:21 +0700

    :D
    semoga dah bener

--- source_code/carirarity.py
+++ source_code/carirarity.py
@@ -1,4 +1,27 @@
-# prosedur akhir
-def carirarity(databases):
-	print("belum")
-	return databases
+def printdata(data):
+    # meng-output setiap data yang terdapat pada array data
+
+    # kamus
+    # data : array of string and integer { array data yang ingin ditampilkan }
+    print("Nama: ", data[1])
+    print("Deskripsi: ", data[2])
+    print("Jumlah: ", data[3], "buah")
+    print("Rarity: ", data[4])
+    print("Tahun Ditemukan: ", data[5])
+
+def carirarity(database):
+    # meng-output data berdasarkan rarity yang dipilih
+
+    # kamus
+    # i : integer { indeks }
+    # rarity : string { input rarity yang ingin ditampilkan oleh user }
+    # array_data : array of array of string and integer { array yang berisi data-data tanpa header }
+    # database : array of array of string and integer { array yang berisi data - data dengan header }
+
+    # algoritma
+    array_data = database.pop(0)
+    rarity = input("Masukkan rarity: ")
+    print("\n" + "Hasil pencarian: " + "\n")
+    for data in array_data:
+        if (array_data[data][4] == rarity):
+            printdata(array_data[data])
--- source_code/caritahun.py
+++ source_code/caritahun.py
@@ -1,13 +1,29 @@
+def printdata(data):
+    # meng-output setiap data yang terdapat pada array data
+
+    # kamus
+    # data : array of string and integer { array data yang ingin ditampilkan }
+    print("Nama: ", data[1])
+    print("Deskripsi: ", data[2])
+    print("Jumlah: ", data[3], "buah")
+    print("Rarity: ", data[4])
+    print("Tahun Ditemukan: ", data[5])
+
 def caritahun(database):
+    # meng-output data gadget berdasarkan tahun ditemukannya
+
+    # kamus
+    # i : integer { indeks }
+    # kategori : string { input kategori yang ingin ditampilkan oleh user }
+    # tahun : integer { input tahun sebagai patokan }
+    # array_data : array of array of string and integer { array yang berisi data-data tanpa header }
+    # database : array of array of string and integer { array yang berisi data - data dengan header }
+    
+    # algoritma
+
     array_data = database.pop(0)
-    def printdata(data):
-        print("Nama: ", data[1])
-        print("Deskripsi: ", data[2])
-        print("Jumlah: ", data[3], "buah")
-        print("Rarity: ", data[4])
-        print("Tahun Ditemukan: ", data[5])
     tahun = int(input("Masukkan tahun: "))
-    kategori = str(input("Masukkan kategori: "))
+    kategori = input("Masukkan kategori: ") # input user berupa ">" , "<" , "=" , "<=" , ">="
     print("\n" + "Hasil pencarian: " + "\n")
     if (kategori == ">"):
         for data in array_data:
