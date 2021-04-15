155032600bcd3043b8473ea89ba1b4b364778035
FlitzyBlue332 <raihanaulia332@gmail.com>
2021-04-15 09:35:53 +0700

    register
    sudah bener?

--- source_code/caritahun.py
+++ source_code/caritahun.py
@@ -1,4 +1,31 @@
-# prosedur akhir
-def caritahun(databases):
-	print("belum")
-	return databases
+def caritahun(database):
+    array_data = database.pop(0)
+    def printdata(data):
+        print("Nama: ", data[1])
+        print("Deskripsi: ", data[2])
+        print("Jumlah: ", data[3], "buah")
+        print("Rarity: ", data[4])
+        print("Tahun Ditemukan: ", data[5])
+    tahun = int(input("Masukkan tahun: "))
+    kategori = str(input("Masukkan kategori: "))
+    print("\n" + "Hasil pencarian: " + "\n")
+    if (kategori == ">"):
+        for data in array_data:
+            if ((array_data[data][5]) > tahun) :
+                printdata(array_data[data])
+    elif (kategori == "="):
+        for data in array_data:
+            if ((array_data[data][5]) == tahun) :
+                printdata(array_data[data])
+    elif (kategori == "<"):
+        for data in array_data:
+            if ((array_data[data][5]) < tahun) :
+                printdata(array_data[data])
+    elif (kategori == ">="):
+        for data in array_data:
+            if ((array_data[data][5]) >= tahun) :
+                printdata(array_data[data])
+    elif (kategori == "<="):
+        for data in array_data:
+            if ((array_data[data][5]) <= tahun) :
+                printdata(array_data[data])
--- source_code/register.py
+++ source_code/register.py
@@ -1,23 +1,21 @@
-# nanti tolong diedit ya fungsinya
-# jadi edit list aja di dalem fungsi ini
-# trus jangan ada def di dalem def, taro di luar aja 
-# fungsi input() udah otomatis string, jadi gausah pake str()
+# Register
 
-def split(string, delimiters):
-    result = []
-    word = ''
-    for c in string:
-        if c not in delimiters:
-            word += c
-        elif word:
-            result.append(word)
-            word = ''
+# meregister akun user ke dalam sistem
 
-    if word:
-        result.append(word)
+# Kamus
+def register_save(new_data):
+    # menyimpan hasil ke file user.csv
+    f = open("user.csv","w")
+    f.write(new_data)
+    f.close()
 
-    return result
 def check_username(username,data):
+    # mengecek apakah username input sudah ada atau belum pada data
+    # kamus
+    # i : integer { indeks }
+    # data : array of array of string and integer
+    # username : string
+    # check = integer
     check = 0
     for i in range(len(data)):
         if (data[i][1] == username):
@@ -26,50 +24,34 @@ def check_username(username,data):
         return True
     else: #check = 1
         return False
-def data_user():
-    f=open("user.csv","r")
-    raw_lines = f.readlines()
-    f.close()
-    lines = [raw_line.replace("\n","") for raw_line in raw_lines]
-    array_of_data_user = [split(line,",;") for line in lines]
-    header = array_of_data_user[0]
-    array_of_data_user.pop(0)
-    return header, array_of_data_user
-def conv_data_string(header,data):
-    string_data = ",".join(header) + "\n"
-    for arr_data in data:
-        arr_data_all_string = [str(var) for var in arr_data]
-        string_data += ",".join(arr_data_all_string)
-        string_data += "\n"
-    return string_data
-def register(array_data): # array data nanti bisa diganti jadi databases, kalo mau akses list user bisa pake databases[user]
-    def register_save(new_data):
-        f = open("user.csv","w")
-        f.write(new_data)
-        f.close()
-    def check_username(username,data):
-        check = 0
-        for i in range(len(data)):
-            if (data[i][1] == username):
-                check = 1
-        if (check == 0):
-            return True
-        else: #check = 1
-            return False
-    head, data = array_data
-    nama = str(input("Masukkan nama: "))
+
+def register(array_data):
+    # Fungsi utama dari register
+
+    # Kamus lokal
+    # i : integer {sebagai indeks}
+    # head : array of string { header pada file data }
+    # nama, username, alamat, password : string {masukan nama user}
+    # id : integer {dibuat otomatis oleh sistem}
+    # role : string { dibuat secara default oleh sistem sebagai user. sehingga register hanya menghasilkan role user }
+    # new_data : array of string
+    # data : array of array of string and integer { data pada file data }
+
+    # algoritma
+
+    head = array_data[0]
+    data = array_data.pop(0)
+    nama = input("Masukkan nama: ")
     nama = nama.title()
-    username = str(input("Masukkan username: "))
+    username = input("Masukkan username: ")
     while (check_username(username,data) == False):
         print("username sudah diambil")
-        username = str(input("Masukkan username: "))
-    password = str(input("Masukkan password: "))
-    alamat = str(input("Masukkan alamat: "))
+        username = input("Masukkan username: ")
+    password = input("Masukkan password: ")
+    alamat = input("Masukkan alamat: ")
     id = len(data) +1
     role = "User"
     new_data = [id,username,nama,alamat,password,role]
     data.append(new_data)
     print("User", username, "telah berhasil register ke dalam Kantong Ajaib")
     register_save(conv_data_string(head,data))
-
-register(data_user())
