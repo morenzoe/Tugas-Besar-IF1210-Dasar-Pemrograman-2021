48e8a80f826c37d105ca3196cb9042ad89960671
khffnsa <82304706+khffnsa@users.noreply.github.com>
2021-04-18 00:42:29 +0700

    Update tambahitem.py

--- TBIF1210-02-10/tambahitem.py
+++ TBIF1210-02-10/tambahitem.py
@@ -13,6 +13,12 @@ def cekid(ID,data):
         else:
                 return False
 
+def cekdelimit(cek):
+	if ";" in cek:
+		return True
+	else:
+		return False
+
 
 def tambahitem(databases):
         fileg = databases[gadget]
@@ -28,13 +34,25 @@ def tambahitem(databases):
                         print("Gagal menambahkan item karena ID sudah ada.")
                 else:
                         nama = input("Masukan Nama : ")
+                        while cekdelimit(nama):
+                                print("Nama tidak valid")
+                                nama = input("Masukan Nama : ")
                         deskripsi = input("Masukan Deskripsi : ")
-                        jumlah = input("Masukan Jumlah : ")
+                        while cekdelimit(deskripsi):
+                                print("Deskripsi tidak valid")
+                                deskripsi = input("Masukan Deksripsi : ")
+                        jumlah = int(input("Masukan Jumlah : "))
+                        while cekdelimit(jumlah):
+                                print("Jumlah tidak valid")
+                                jumlah = input("Masukan Jumlah : ")
                         rarity = input("Masukan Rarity : ")
                         while rarity not in "CBAS":
                                 print("Input rarity tidak valid")
-                                rarity = input("Masukan rarity: ")
+                                rarity = input("Masukan Rarity: ")
                         tahun = int(input("Masukan tahun ditemukan : "))
+                        while cekdelimit(tahun):
+                                print("Tahun tidak valid")
+                                tahun = input("Masukan Tahun : ")
                         new_gadget = [ID,nama,deskripsi,jumlah,rarity,tahun]
                         datag.append(new_gadget)
                         databases[gadget]=datag
@@ -45,12 +63,21 @@ def tambahitem(databases):
                         print("Gagal menambahkan item karena ID sudah ada.")
                 else:
                         nama = input("Masukan Nama : ")
+                        while cekdelimit(nama):
+                                print("Nama tidak valid")
+                                nama = input("Masukan Nama : ")
                         deskripsi = input("Masukan Deskripsi : ")
-                        jumlah = input("Masukan Jumlah : ")
+                        while cekdelimit(deskripsi):
+                                print("Deskripsi tidak valid")
+                                deskripsi = input("Masukan Deksripsi : ")
+                        jumlah = int(input("Masukan Jumlah : "))
+                        while cekdelimit(jumlah):
+                                print("Jumlah tidak valid")
+                                jumlah = input("Masukan Jumlah : ")
                         rarity = input("Masukan Rarity : ")
                         while rarity not in "CBAS":
                                 print("Input rarity tidak valid")
-                                rarity = input("Masukan rarity: ")
+                                rarity = input("Masukan Rarity: ")
                         new_consumable = [ID,nama,deskripsi,jumlah,rarity]
                         datac.append(new_consumable)
                         databases[consumable]=datac
