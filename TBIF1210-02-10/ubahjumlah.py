30ab33e6be43e6f840b3476dc820975df67aa6e9
khffnsa <82304706+khffnsa@users.noreply.github.com>
2021-04-18 11:03:54 +0700

    Update ubahjumlah.py

--- TBIF1210-02-10/ubahjumlah.py
+++ TBIF1210-02-10/ubahjumlah.py
@@ -1,4 +1,67 @@
+from constant import gadget
+from constant import consumable
+
 # prosedur akhir
+
+def cekid(ID,data):
+        cek = 0
+        for i in range(len(data)):
+                if data[i][0]==ID :
+                        cek = 1
+        if cek == 1 :
+                return True
+        else:
+                return False
+
+def ubah(ID,data, jumlah):
+        for i in range (len(data)):
+                if data[i][0] == ID :
+                        idx = 1
+
+        stok = data[idx][3] + jumlah
+        
+        if stok >= 0 :
+                data[idx][3] = stok
+        else :
+                data[idx][3] = stok - jumlah
+         
+                
+
+        if data[idx][3] >= 0 :
+                if jumlah >= 0 :
+                        print(jumlah , (data[idx][1]) , "berhasil ditambahkan. Stok sekarang : ",(data[idx][3]))
+                else :
+                        print(jumlah , (data[idx][1]) , "berhasil dibuang. Stok sekarang : ",(data[idx][3]))
+        else :
+                print(jumlah , (data[idx][1]) , "gagal dibuang karena stok kurang. Stok sekarang : ", (data[idx][3]))
+
+        
+        
+        
+
 def ubahjumlah(databases):
-	print("belum")
-	return databases
+        fg = databases[gadget]
+        datag = fg
+        fc = databases[consumable]
+        datac = fc
+
+        ID = input("Masukan ID : ")
+        jumlah = int(input("Masukan Jumlah : "))
+
+        if ID[0] == "G" :
+                if cekid(ID,datag) :
+                        ubah(ID,datag,jumlah)
+                else :
+                        print("Gagal mengubah jumlah karena ID gadget tidak ditemukan.")
+        elif ID[0] == "C" :
+                if cekid(ID,datac) :
+                        ubah(ID,datac,jumlah)
+                else : print("Gagal mengubah jumlah karena ID consumable tidak ditemukan.")
+        else :
+                print("Gagal mengubah jumlah karena ID tidak valid.")
+                        
+
+
+
+
+        return databases
