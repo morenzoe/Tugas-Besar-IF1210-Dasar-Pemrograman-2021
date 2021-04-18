867aee0c181f0e376e23be452c362515c3b6514d
Eraraya Morenzo <morenzomuten@gmail.com>
2021-04-18 09:37:40 +0700

    Update riwayatkembali.py

--- TBIF1210-02-10/riwayatkembali.py
+++ TBIF1210-02-10/riwayatkembali.py
@@ -1,4 +1,20 @@
-# prosedur akhir
+from constant import gadget_return_history
+
+def print_baris(row):
+	print("ID Pengambilan:")
+	print("Nama Pengambil: ")
+	print("Nama Gadget: ")
+	print("Tanggal Pengambilan: ")
+
 def riwayatkembali(databases):
+	#buat list baru
+		#['id', 'id_peminjam', 'id_gadget', 'tanggal_pengembalian'],
+	#urutin
+	#print per 5 pake counter, tanya buat lanjut
+	db_return = databases[gadget_return_history]
+	db_user = databases[user]
+	db_gadget = databases[gadget]
+	for row in data:
+		row[1] = 
 	print("belum")
 	return databases
