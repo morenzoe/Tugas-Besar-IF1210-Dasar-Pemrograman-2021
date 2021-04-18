d642735af30db8c056678f6fdcf3df4028f3cd4d
FlitzyBlue332 <raihanaulia332@gmail.com>
2021-04-18 16:52:36 +0700

    Update riwayatpinjam.py

--- TBIF1210-02-10/riwayatpinjam.py
+++ TBIF1210-02-10/riwayatpinjam.py
@@ -18,9 +18,7 @@ def sortMaxMinTanggal(array):
             array[imax],array[Pass]=array[Pass],array[imax]
     return array
 def nama_user_id(database,id):
-    for data in range(1,len(database)):
-        if id == database[data][0]:
-            return database[data][2]
+    return database[id][2]
 def nama_gadget_id(database,id):
     for data in range(1,len(database)):
         if id == database[data][0]:
@@ -31,25 +29,42 @@ def printdata(nama_user,nama_gadget,database):
     print("Nama Gadget:", nama_gadget)
     print("Tanggal Peminjaman:", database[3])
     print("Jumlah:", database[4])
+
 def riwayatpinjam(databases):
     if databases[6][5] == "User" :
         print("maafkan saya", databases[6][2] + "-san", "saya tidak dapat mengizinkan anda menggunakan command ini")
         return databases
     else:
         print("selamat datang", databases[6][2] + "-san", "berikut adalah data riwayat peminjaman gadget")
-        data_sorted = sortMaxMinTanggal(databases[1])
-        awal = 1
-        akhir = 6
+        print()
+        sorted_history = sortMaxMinTanggal(databases[4][1:])
+        db_gadget = databases[1]
+        db_user = databases[0]
+        username = databases[6][1]
+        awal = 0
+        akhir = 5
         repeat = "y"
-        while ((repeat == "y") or (repeat == "Y")):
-            if akhir < len(data_sorted):
+        while (repeat == "Y") or (repeat == "y"):
+            if akhir < len(sorted_history):
                 for data in range(awal,akhir):
-                    printdata(nama_user_id(databases[0],data_sorted[i][0]),(nama_gadget_id(databases[0],data_sorted[i][0])), data_sorted)
-                repeat = input("apakah anda ingin melihat selanjutnya 5 data selanjutnya", database[6][2] + "-san ?")
-                if (repeat == "y") or (repeat == "Y"):
-                    awal = akhir
-                    akhir = akhir + 5
-            elif akhir >= len(data_sorted):
-                for data in range(awal,len(data_sorted)):
-                    printdata(nama_user_id(databases[0],data_sorted[i][0]),(nama_gadget_id(databases[0],data_sorted[i][0])), data_sorted)
+                    nama_user = nama_user_id(db_user, sorted_history[data][1])
+                    nama_gadget = nama_gadget_id(db_gadget, sorted_history[data][2])
+                    printdata(nama_user,nama_gadget,sorted_history[data])
+                    print()
+                awal = akhir
+                akhir += 5
+                repeat = input("Apakah anda ingin melihat halaman selanjutnya? (Y/N): ")
+                print()
+                while (not (repeat in "YyNn") and len(repeat)!=1):
+                    repeat = input("input", username+"-san salah mohon masukkan input yang benar (Y/N): ")
+                    print()
+            elif akhir >= len(sorted_history):
+                for data in range(awal,len(sorted_history)):
+                    nama_user = nama_user_id(db_user, sorted_history[data][1])
+                    nama_gadget = nama_gadget_id(db_gadget, sorted_history[data][2])
+                    printdata(nama_user,nama_gadget,sorted_history[data])
+                    print()
+                repeat = "N"
     return databases
+
+# database : [['2', 1, 'G2', '20/04/2021', 1], ['1', 1, 'G1', '10/04/2021', 3]]
