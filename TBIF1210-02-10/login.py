4f79b76f1895f0bf9bd3446e2049207b2359e2ff
khafifanisa <82304706+khffnsa@users.noreply.github.com>
2021-04-24 20:39:21 +0700

    Update login.py

--- TBIF1210-02-10/login.py
+++ TBIF1210-02-10/login.py
@@ -7,19 +7,29 @@ def login(databases):
 
 	file = databases[user]
 
-	find = False
+	find_u = False
+	find_p = False
 
 	for row in file:
-		if row[1] == username and row[4] == password:
-			find = True
-			break
+		if row[1] == username :
+                        if row[4] == password:
+                                find_u = True
+                                find_p = True
+                                break
+                        else :
+                                find_u = True
+                                find_p = False
 		else:
-			find = False
-	if find == True :
+			find_u = False
+			
+	if find_u == True and find_p == True :
 		databases.append(row)
 		print("Login berhasil!")
-	else:
-		print("Login gagal! Username atau password salah.") 
+	elif find_p == False :
+                print("Login gagal! Password kamu salah.")
+                
+	elif find_u == False :
+		print("Login gagal! Username kamu tidak ditemukan.") 
 		# mungkin nanti bisa dipisah. find buat username, setelah itu baru bandingin lagi passwordnya
 		
 	return databases
