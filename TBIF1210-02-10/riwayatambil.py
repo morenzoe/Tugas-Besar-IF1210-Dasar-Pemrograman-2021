72a2366136fef19f8bfe4e5d99960e2a7db813a7
FlitzyBlue332 <raihanaulia332@gmail.com>
2021-04-25 20:55:09 +0700

    Added cek_active_account

--- TBIF1210-02-10/riwayatambil.py
+++ TBIF1210-02-10/riwayatambil.py
@@ -1,7 +1,8 @@
 # prosedur akhir
 import datetime
 from riwayatpinjam import sortMaxMinTanggal
-
+from login import cek_active_account
+from constant import gadget,user,consumable_history,active_account,consumable
 def nama_user_id(database,id):
     # menghasilkan nama dari user dari id tersebut
     return database[id][2]
@@ -19,38 +20,41 @@ def printdata(nama_user,nama_consumeable,database):
 
 def riwayatambil(databases):
     # menampilkan data pada "consumeable_borrow_history.csv" secara berurutan berdasarkan tanggal
-    if databases[6][5] == "User" :
-        print("maafkan saya", databases[6][2] + "-san", "saya tidak dapat mengizinkan anda menggunakan command ini")
-        return databases
+    isLoggedIn = cek_active_account(databases)
+    if isLoggedIn:
+        if databases[active_account][5] == "User" :
+            print("maafkan saya", databases[active_account][2] + "-san", "saya tidak dapat mengizinkan anda menggunakan command ini")
+        else:
+            print("selamat datang", databases[active_account][2] + "-san", "berikut adalah data riwayat peminjaman consumeable")
+            print()
+            sorted_history = sortMaxMinTanggal(databases[consumable_history][1:],3)
+            db_consumeable = databases[consumable]
+            db_user = databases[user]
+            username = databases[active_account][1]
+            awal = 0
+            akhir = 5
+            repeat = "y"
+            while (repeat == "Y") or (repeat == "y"):
+                if akhir < len(sorted_history):
+                    for data in range(awal,akhir):
+                        nama_user = nama_user_id(db_user, sorted_history[data][1])
+                        nama_consumeable = nama_consumeable_id(db_consumeable, sorted_history[data][2])
+                        printdata(nama_user,nama_consumeable,sorted_history[data])
+                        print()
+                    awal = akhir
+                    akhir += 5
+                    repeat = input("Apakah anda ingin melihat halaman selanjutnya? (Y/N): ")
+                    print()
+                    while (not (repeat in "YyNn") and len(repeat)!=1):
+                        repeat = input("input", username+"-san salah mohon masukkan input yang benar (Y/N): ")
+                        print()
+                elif akhir >= len(sorted_history):
+                    for data in range(awal,len(sorted_history)):
+                        nama_user = nama_user_id(db_user, sorted_history[data][1])
+                        nama_consumeable = nama_consumeable_id(db_consumeable, sorted_history[data][2])
+                        printdata(nama_user,nama_consumeable,sorted_history[data])
+                        print()
+                    repeat = "N"
     else:
-        print("selamat datang", databases[6][2] + "-san", "berikut adalah data riwayat peminjaman consumeable")
-        print()
-        sorted_history = sortMaxMinTanggal(databases[3][1:],3)
-        db_consumeable = databases[2]
-        db_user = databases[0]
-        username = databases[6][1]
-        awal = 0
-        akhir = 5
-        repeat = "y"
-        while (repeat == "Y") or (repeat == "y"):
-            if akhir < len(sorted_history):
-                for data in range(awal,akhir):
-                    nama_user = nama_user_id(db_user, sorted_history[data][1])
-                    nama_consumeable = nama_consumeable_id(db_consumeable, sorted_history[data][2])
-                    printdata(nama_user,nama_consumeable,sorted_history[data])
-                    print()
-                awal = akhir
-                akhir += 5
-                repeat = input("Apakah anda ingin melihat halaman selanjutnya? (Y/N): ")
-                print()
-                while (not (repeat in "YyNn") and len(repeat)!=1):
-                    repeat = input("input", username+"-san salah mohon masukkan input yang benar (Y/N): ")
-                    print()
-            elif akhir >= len(sorted_history):
-                for data in range(awal,len(sorted_history)):
-                    nama_user = nama_user_id(db_user, sorted_history[data][1])
-                    nama_consumeable = nama_consumeable_id(db_consumeable, sorted_history[data][2])
-                    printdata(nama_user,nama_consumeable,sorted_history[data])
-                    print()
-                repeat = "N"
+        print("Anda belum login ≥﹏≤")
     return databases
