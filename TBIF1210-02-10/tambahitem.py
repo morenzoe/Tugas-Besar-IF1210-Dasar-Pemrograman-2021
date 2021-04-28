821449d5d8ce3373b6879e5f2359f1a5db6e7111
khafifanisa <82304706+khffnsa@users.noreply.github.com>
2021-04-28 16:34:38 +0700

    Update tambahitem.py.

--- TBIF1210-02-10/tambahitem.py
+++ TBIF1210-02-10/tambahitem.py
@@ -35,98 +35,124 @@ def tambahitem(databases):
                  return databases
             else :
                 if ID[0]=="G" :
-                    if cekid(ID,file_gadget) :
-                        nama = input("Masukan Nama : ")
-                        if cekdelimit(nama):
-                            print("Nama tidak valid.")
-                            return databases
-                            
-                        deskripsi = input("Masukan Deskripsi : ")
-                        if cekdelimit(deskripsi):
-                            print("Deskripsi tidak valid.")
-                            return databases
-                            
-                        jumlah = input("Masukan Jumlah : ")
-                        if cekdelimit(jumlah):
-                            print("Jumlah tidak valid.")
-                            return databases
-                        try :
-                            jumlah = int(jumlah)
-                        except :
-                            print("Jumlah harus berupa bilanhan bulat positif.")
-                            return databases
-                        if jumlah <= 0 :
-                            print("Jumlah harus bilangan bulat positif.")
-                            return databases
-                        
-                        rarity = input("Masukan Rarity : ")
-                        pjg = len(rarity)
-                        if pjg == 1:
-                            if rarity not in "CBAS" :
-                                print("Rarity tidak valid.")
-                                return databases
-                        else :
-                            print("Rarity tidak valid.")
-                            return databases
-
-                        tahun = input("Masukan tahun ditemukan : ")
-                        if cekdelimit(tahun):
-                            try :
-                                tahun = int(tahun)
-                            except :
-                                print("Tahun tidak valid.")
-                                return databases 
-                        
-                        new_gadget = [ID,nama,deskripsi,jumlah,rarity,tahun]
-                        file_gadget.append(new_gadget)
-                        databases[gadget] = file_gadget
-                        print("Item telah berhasil ditambahkan ke database.")
-                        
+                    if pjgID == 1 :
+                        print("Gagal menambah item karena ID gadget tidak valid.")
                     else :
-                        print("Gagal menambahkan gadget, karena ID sudah ada.")
+                        Id = ID[1:]
+                        try :
+                            Id = int(Id)
+                        except :
+                            print("Gagal menambah item karena ID gadget tidak valid.")
+                            return databases
+                        
+                        if Id <= 0 :
+                            print("Gagal menambah item karena ID gadget tidak valid.")
+                            return databases
+                    
+                        if cekid(ID,file_gadget) :
+                            nama = input("Masukan Nama : ")
+                            if cekdelimit(nama):
+                                print("Nama tidak valid.")
+                                return databases
+                            
+                            deskripsi = input("Masukan Deskripsi : ")
+                            if cekdelimit(deskripsi):
+                                print("Deskripsi tidak valid.")
+                                return databases
+                            
+                            jumlah = input("Masukan Jumlah : ")
+                            if cekdelimit(jumlah):
+                                print("Jumlah tidak valid.")
+                                return databases
+                            try :
+                                jumlah = int(jumlah)
+                            except :
+                                print("Jumlah harus berupa bilangan bulat positif.")
+                                return databases
+                            if jumlah <= 0 :
+                                print("Jumlah harus bilangan bulat positif.")
+                                return databases
+                        
+                            rarity = input("Masukan Rarity : ")
+                            pjg = len(rarity)
+                            while True :
+                                if pjg == 1 and rarity in "CBAS":
+                                    break
+                                else :
+                                    print("Rarity tidak valid.")
+                                    return databases
+
+                            tahun = input("Masukan tahun ditemukan : ")
+                            if cekdelimit(tahun):
+                                try :
+                                    tahun = int(tahun)
+                                except :
+                                    print("Tahun tidak valid.")
+                                    return databases 
+                        
+                            new_gadget = [ID,nama,deskripsi,jumlah,rarity,tahun]
+                            file_gadget.append(new_gadget)
+                            databases[gadget] = file_gadget
+                            print("Item telah berhasil ditambahkan ke database.")
+                        
+                        else :
+                            print("Gagal menambahkan gadget, karena ID sudah ada.")
                         
                 elif ID[0] == "C" :
-                    if cekid(ID,file_consumable) :
-                        nama = input("Masukan Nama : ")
-                        if cekdelimit(nama):
-                            print("Nama tidak valid.")
-                            return databases
-                            
-                        deskripsi = input("Masukan Deskripsi : ")
-                        if cekdelimit(deskripsi):
-                            print("Deskripsi tidak valid.")
-                            return databases
-                            
-                        jumlah = input("Masukan Jumlah : ")
-                        if cekdelimit(jumlah):
-                            print("Jumlah tidak valid.")
-                            return databases
-                        try :
-                            jumlah = int(jumlah)
-                        except :
-                            print("Jumlah harus berupa bilanhan bulat positif.")
-                            return databases
-                        if jumlah <= 0 :
-                            print("Jumlah harus bilangan bulat positif.")
-                            return databases
-                        
-                        rarity = input("Masukan Rarity : ")
-                        pjg = len(rarity)
-                        if pjg == 1:
-                            if rarity not in "CBAS" :
-                                print("Rarity tidak valid.")
-                                return databases
-                        else :
-                            print("Rarity tidak valid.")
-                            return databases
-                            
-                        new_consumable = [ID,nama,deskripsi,jumlah,rarity]
-                        file_consumable.append(new_consumable)
-                        databases[consumable] = file_consumable
-                        print("Item telah berhasil ditambahkan ke database.")
-                    
+                    if pjgID == 1 :
+                        print("Gagal menambah item karena ID consumable tidak valid.")
                     else :
-                        print("Gagal menambahkan consumable karena ID sudah ada.")                    
+                        Id = ID[1:]
+                        try :
+                            Id = int(Id)
+                        except :
+                            print("Gagal menambah item karena ID consumable tidak valid.")
+                            return databases 
+                        
+                        if Id <= 0 :
+                            print("Gagal menambah item karena ID gadget tidak valid.")
+                            return databases
+                    
+                        if cekid(ID,file_gadget) :
+                            nama = input("Masukan Nama : ")
+                            if cekdelimit(nama):
+                                print("Nama tidak valid.")
+                                return databases
+                            
+                            deskripsi = input("Masukan Deskripsi : ")
+                            if cekdelimit(deskripsi):
+                                print("Deskripsi tidak valid.")
+                                return databases
+                            
+                            jumlah = input("Masukan Jumlah : ")
+                            if cekdelimit(jumlah):
+                                print("Jumlah tidak valid.")
+                                return databases
+                            try :
+                                jumlah = int(jumlah)
+                            except :
+                                print("Jumlah harus berupa bilangan bulat positif.")
+                                return databases
+                            if jumlah <= 0 :
+                                print("Jumlah harus bilangan bulat positif.")
+                                return databases
+                        
+                            rarity = input("Masukan Rarity : ")
+                            pjg = len(rarity)
+                            while True :
+                                if pjg == 1 and rarity in "CBAS":
+                                    break
+                                else :
+                                    print("Rarity tidak valid.")
+                                    return databases
+                            
+                            new_consumable = [ID,nama,deskripsi,jumlah,rarity]
+                            file_consumable.append(new_consumable)
+                            databases[consumable] = file_consumable
+                            print("Item telah berhasil ditambahkan ke database.")
+                    
+                        else :
+                            print("Gagal menambahkan consumable karena ID sudah ada.")                    
 
                 else :
                     print("Gagal menambahkan item karena ID tidak valid.")
