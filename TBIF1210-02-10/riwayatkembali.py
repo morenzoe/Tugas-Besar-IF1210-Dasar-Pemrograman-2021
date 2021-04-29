095b5d684ee73ecf8f21297e0a4457aae7cb858f
Eraraya Morenzo <morenzomuten@gmail.com>
2021-04-29 16:01:37 +0700

    Added comments.

--- TBIF1210-02-10/riwayatkembali.py
+++ TBIF1210-02-10/riwayatkembali.py
@@ -1,28 +1,127 @@
-"""Program F12 - Melihat Riwayat Pengembalian Gadget 
-Prosedur ini akan menampilkan riwayat pengembalian gadget berisi data 
-ID Pengambil, Nama Pengambil, Nama Gadget, dan Tanggal Pengambilan.
+"""Program F12 - Melihat Riwayat Pengembalian Gadget
+Prosedur ini akan menampilkan riwayat pengembalian gadget berisi data
+ID Pengambil, Nama Pengambil, Nama Gadget, dan Tanggal Pengambilan.  
 Prosedur ini akan menampilkan riwayat pengembalian yang terurut menurun
-dari yang paling terbaru. Prosedur menampilkan 5 data sekali keluaran.
+dari yang paling terbaru.  Prosedur menampilkan tiap lima data sekali.
+
+Akses : Admin
 """
 
 # KAMUS
-# standard library imports
+# Standard library imports
 import datetime
 
-# local library imports
+# Local library imports
 from constant import user,gadget,gadget_borrow_history, \
                      gadget_return_history,active_account
 from login import cek_active_account
-from riwayatpinjam import sortMaxMinTanggal,nama_user_id,nama_gadget_id	
+from riwayatpinjam import sortMaxMinTanggal,nama_user_id,nama_gadget_id
 
-# variable
-#
+# Variable
+# isLoggedIn : bool
+# username, role, : str
+# db_gadget, db_kembali, db_pinjam : list of list
+# db_user, sorted_db_kembali : list of list
+# list_tampilan : list of list
 
-# function and procedure
-#
+def print_baris(row):
+    """Prosedur ini akan menampilkan data pada list sesuai dengan
+    tampilan berupa:
+    ID Pengambilan      :
+    Nama Pengambil      :
+    Nama Gadget         :
+    Tanggal Pengembalian:
+    """
+
+    # KAMUS LOKAL
+    # -
+
+    # ALGORITMA
+    print("ID Pengambilan      :",row[0])
+    print("Nama Pengambil      :",row[1])
+    print("Nama Gadget         :",row[2])
+    print("Tanggal Pengembalian:",row[3])
+
+def tampilan(list_tampilan,username):
+    """Fungsi ini akan menerima input list of list berisi data yang
+    akan ditampilkan per lima baris dan akan dilanjutkan sesuai
+    keinginan user.
+    """
+    
+    # KAMUS LOKAL
+    # lanjut : str    
+    
+    # ALGORITMA
+    # Menampilkan sebanyak data pada list tampilan
+    for i in range(len(list_tampilan)):
+        print_baris(list_tampilan[i])
+        
+        # Menampilkan baris kosong sebagai pemisah antar data
+        if i!=len(list_tampilan)-1:
+            print()
+        
+        # Setelah menampilkan lima data, user memilih lanjut atau tidak
+        if (i+1)%5==0 and i!=len(list_tampilan)-1:
+            # Validasi input dari user
+            while True:
+                lanjut = input("Apakah "+username+" mau melihat \
+                                riwayat berikutnya? (Y/N) ")
+                                
+                # Input valid, salah satu huruf Y/y/N/n
+                if lanjut in "YyNn" and len(lanjut)==1:
+                    break
+                # Input tidak valid, pengisian diulang
+                elif lanjut not in "YyNn" or len(lanjut)!=1:
+                    print("Input tidak sesuai. Ulangi!")
+                    print()
+            
+            # Tidak melanjutkan penampilan data berikutnya, terminasi loop
+            if lanjut in "Nn":
+                break
+            # Melanjutkan penampilan lima data berikutnya
+            elif lanjut in "Yy":
+                print()
+
+def buat_list_tampilan(kembali, pinjam, user, gadget):
+    """Fungsi ini akan menerima input list berisi data pengembalian,
+    peminjaman, user, dan gadget.  Kemudian fungsi mengembalikan
+    sebuah list berisi data ID Pengambilan, Nama Pengambil, Nama
+    Gadget, dan Tanggal Pengembalian yang sesuai pada tiap 
+    data pengembalian.
+    """
+    
+    # KAMUS LOKAL
+    # kembali_copy : list of list
+    # row_copy : list of string and int
+    # id_pengambilan, id_peminjam : int
+    # nama_pengambil, id_gadget, nama_gadget, tanggal_pengembalian : str
+    
+    # ALGORITMA
+    # inisialisasi list kosong
+    kembali_copy = []
+    
+    # mencari data yang sesuai untuk tiap list pada riwayat pengembalian
+    for row in kembali:
+        id_pengambilan = row[1]
+        id_peminjam = pinjam[id_pengambilan][1]
+        nama_pengambil = nama_user_id(user,id_peminjam)
+        id_gadget = pinjam[id_pengambilan][2]
+        nama_gadget = nama_gadget_id(gadget,id_gadget)
+        tanggal_pengembalian = row[2]
+		
+        # membuat list baru dengan data yang sesuai untuk satu peminjaman
+        row_copy = [id_pengambilan, nama_pengambil, 
+                    nama_gadget, tanggal_pengembalian]
+        
+        # mengumpulkan list dengan data yang sesuai dari tiap peminjaman
+        kembali_copy.append(row_copy)
+    
+    # mengembalikan list of list dengan data yang akan ditampilkan
+    return kembali_copy
 
 # ALGORITMA PROGRAM UTAMA
 def riwayatkembali(databases):
+    # 
 	isLoggedIn = cek_active_account(databases)
 	if isLoggedIn:
 		username = databases[active_account][1]
@@ -47,46 +146,3 @@ def riwayatkembali(databases):
 	else:
 		print("Silahkan login terlebih dahulu.")
 		return databases
-
-# REALISASI FUNGSI DAN PROSEDUR
-
-def print_baris(row):
-	print("ID Pengambilan:",row[0])
-	print("Nama Pengambil:",row[1])
-	print("Nama Gadget:",row[2])
-	print("Tanggal Pengembalian:",row[3])
-
-def buat_list_tampilan(kembali, pinjam, user, gadget):
-	kembali_copy = []
-	for row in kembali:
-		id_pengambilan = row[1]
-		id_peminjam = pinjam[id_pengambilan][1]
-		nama_pengambil = nama_user_id(user,id_peminjam)
-		id_gadget = pinjam[id_pengambilan][2]
-		nama_gadget = nama_gadget_id(gadget,id_gadget)
-		tanggal_pengembalian = row[2]
-		
-		row_copy = [id_pengambilan, nama_pengambil, nama_gadget, tanggal_pengembalian]
-		kembali_copy.append(row_copy)
-	return kembali_copy
-
-def tampilan(list_tampilan,username):
-	for i in range(len(list_tampilan)):
-				print_baris(list_tampilan[i])
-
-				if i!=len(list_tampilan)-1:
-					print()
-					
-				if (i+1)%5==0 and i!=len(list_tampilan)-1:
-					while True:
-						lanjut = input("Apakah "+username+" mau melihat riwayat berikutnya? (Y/N) ")
-						if lanjut in "YyNn" and len(lanjut)==1:
-							break
-						elif lanjut not in "YyNn" or len(lanjut)!=1:
-							print("Input tidak sesuai. Ulangi!")
-							print()
-						
-					if lanjut in "Nn":
-						break
-					elif lanjut in "Yy":
-						print()
