e6c20ae87b3f50be456b8508531d8f8e69d577a1
Eraraya Morenzo <morenzomuten@gmail.com>
2021-04-29 17:15:52 +0700

    Tested autopep8.

--- TBIF1210-02-10/riwayatkembali.py
+++ TBIF1210-02-10/riwayatkembali.py
@@ -1,6 +1,6 @@
 """Program F12 - Melihat Riwayat Pengembalian Gadget
 Prosedur ini akan menampilkan riwayat pengembalian gadget berisi data
-ID Pengambil, Nama Pengambil, Nama Gadget, dan Tanggal Pengambilan.  
+ID Pengambil, Nama Pengambil, Nama Gadget, dan Tanggal Pengambilan.
 Prosedur ini akan menampilkan riwayat pengembalian yang terurut menurun
 dari yang paling terbaru.  Prosedur menampilkan tiap lima data sekali.
 
@@ -12,10 +12,10 @@ Akses : Admin
 import datetime
 
 # Local library imports
-from constant import user,gadget,gadget_borrow_history, \
-                     gadget_return_history,active_account
+from constant import user, gadget, gadget_borrow_history, \
+                     gadget_return_history, active_account
 from login import cek_active_account
-from riwayatpinjam import sortMaxMinTanggal,nama_user_id,nama_gadget_id
+from riwayatpinjam import sortMaxMinTanggal, nama_user_id, nama_gadget_id
 
 # Variable
 # isLoggedIn : bool
@@ -23,6 +23,7 @@ from riwayatpinjam import sortMaxMinTanggal,nama_user_id,nama_gadget_id
 # db_gadget, db_kembali, db_pinjam : list of list
 # db_user, sorted_db_kembali : list of list
 # list_tampilan : list of list
+
 
 def print_baris(row):
     """Prosedur ini akan menampilkan data pada list sesuai dengan
@@ -37,44 +38,45 @@ def print_baris(row):
     # -
 
     # ALGORITMA
-    print("ID Pengambilan      :",row[0])
-    print("Nama Pengambil      :",row[1])
-    print("Nama Gadget         :",row[2])
-    print("Tanggal Pengembalian:",row[3])
+    print("ID Pengambilan      :", row[0])
+    print("Nama Pengambil      :", row[1])
+    print("Nama Gadget         :", row[2])
+    print("Tanggal Pengembalian:", row[3])
 
-def tampilan(list_tampilan,username):
+
+def tampilan(list_tampilan, username):
     """Fungsi ini akan menerima input list of list berisi data yang
     akan ditampilkan per lima baris dan akan dilanjutkan sesuai
     keinginan user.
     """
-    
+
     # KAMUS LOKAL
-    # lanjut : str    
-    
+    # lanjut : str
+
     # ALGORITMA
     # Menampilkan sebanyak data pada list tampilan
     for i in range(len(list_tampilan)):
         print_baris(list_tampilan[i])
-        
+
         # Menampilkan baris kosong sebagai pemisah antar data
-        if i!=len(list_tampilan)-1:
+        if i != len(list_tampilan) - 1:
             print()
-        
+
         # Setelah menampilkan lima data, user memilih lanjut atau tidak
-        if (i+1)%5==0 and i!=len(list_tampilan)-1:
+        if (i + 1) % 5 == 0 and i != len(list_tampilan) - 1:
             # Validasi input dari user
             while True:
-                lanjut = input("Apakah "+username+" mau melihat \
+                lanjut = input("Apakah " + username + " mau melihat \
                                 riwayat berikutnya? (Y/N) ")
-                                
+
                 # Input valid, salah satu huruf Y/y/N/n
-                if lanjut in "YyNn" and len(lanjut)==1:
+                if lanjut in "YyNn" and len(lanjut) == 1:
                     break
                 # Input tidak valid, pengisian diulang
-                elif lanjut not in "YyNn" or len(lanjut)!=1:
+                elif lanjut not in "YyNn" or len(lanjut) != 1:
                     print("Input tidak sesuai. Ulangi!")
                     print()
-            
+
             # Tidak melanjutkan penampilan data berikutnya, terminasi loop
             if lanjut in "Nn":
                 break
@@ -82,54 +84,56 @@ def tampilan(list_tampilan,username):
             elif lanjut in "Yy":
                 print()
 
+
 def buat_list_tampilan(kembali, pinjam, user, gadget):
     """Fungsi ini akan menerima input list berisi data pengembalian,
     peminjaman, user, dan gadget.  Kemudian fungsi mengembalikan
     sebuah list berisi data ID Pengambilan, Nama Pengambil, Nama
-    Gadget, dan Tanggal Pengembalian yang sesuai pada tiap 
+    Gadget, dan Tanggal Pengembalian yang sesuai pada tiap
     data pengembalian.
     """
-    
+
     # KAMUS LOKAL
     # kembali_copy : list of list
     # row_copy : list of string and int
     # id_pengambilan, id_peminjam : int
     # nama_pengambil, id_gadget, nama_gadget, tanggal_pengembalian : str
-    
+
     # ALGORITMA
     # inisialisasi list kosong
     kembali_copy = []
-    
+
     # mencari data yang sesuai untuk tiap list pada riwayat pengembalian
     for row in kembali:
         id_pengambilan = row[1]
         id_peminjam = pinjam[id_pengambilan][1]
-        nama_pengambil = nama_user_id(user,id_peminjam)
+        nama_pengambil = nama_user_id(user, id_peminjam)
         id_gadget = pinjam[id_pengambilan][2]
-        nama_gadget = nama_gadget_id(gadget,id_gadget)
+        nama_gadget = nama_gadget_id(gadget, id_gadget)
         tanggal_pengembalian = row[2]
-		
+
         # membuat list baru dengan data yang sesuai untuk satu peminjaman
-        row_copy = [id_pengambilan, nama_pengambil, 
+        row_copy = [id_pengambilan, nama_pengambil,
                     nama_gadget, tanggal_pengembalian]
-        
+
         # mengumpulkan list dengan data yang sesuai dari tiap peminjaman
         kembali_copy.append(row_copy)
-    
+
     # mengembalikan list of list dengan data yang akan ditampilkan
     return kembali_copy
+
 
 # ALGORITMA PROGRAM UTAMA
 def riwayatkembali(databases):
     # Validasi user sudah login
 	isLoggedIn = cek_active_account(databases)
-    
+
     # User sudah login, validasi role
 	if isLoggedIn:
         # Mendapatkan data terkait user
 		username = databases[active_account][1]
 		role = databases[active_account][5]
-        
+
         # Role user adalah admin, jalankan prosedur riwayatkembali
 		if role == "Admin":
             # Membuat list untuk tiap data yang dibutuhkan
@@ -137,24 +141,26 @@ def riwayatkembali(databases):
 			db_pinjam = databases[gadget_borrow_history]
 			db_gadget = databases[gadget]
 			db_user = databases[user]
-            
+
             # Mengurutkan data pengembalian menurun berdasarkan tanggal
-			sorted_db_kembali = sortMaxMinTanggal(db_kembali,2)
-			
+			sorted_db_kembali = sortMaxMinTanggal(db_kembali, 2)
+
             # Mencari data yang sesuai dan membuat list baru untuk ditampilkan
-			list_tampilan = buat_list_tampilan(sorted_db_kembali,db_pinjam,db_user,db_gadget)
-            
+			list_tampilan = buat_list_tampilan(sorted_db_kembali, db_pinjam,
+                                               db_user, db_gadget)
+
             # Menampilkan data yang sesuai dengan tampilan yang rapih
-			tampilan(list_tampilan,username)
-			
+			tampilan(list_tampilan, username)
+
             # Mengembalikan databases yang sudah digunakan
 			return databases
-		
+
         # Role user bukan admin, terminate prosedur riwayatkembali
 		else:
-			print("Role "+username+" bukan Admin, silahkan login akun Admin.")
+			print("Role " + username + " bukan Admin, silahkan \
+                   login akun Admin.")
 			return databases
-            
+
 	# User belum login, terminate prosedur riwayatkembali
     else:
 		print("Silahkan login terlebih dahulu.")
--- TBIF1210-02-10/save.py
+++ TBIF1210-02-10/save.py
@@ -2,63 +2,68 @@ import os
 from constant import *
 from cek_csv import cek_folder
 
+
 def to_str(array_word_int):
-# menghasilkan word dalam type str
+    # menghasilkan word dalam type str
 
-# KAMUS LOKAL
-# Variabel
-# array_word : array of string
+    # KAMUS LOKAL
+    # Variabel
+    # array_word : array of string
 
-# ALGORITMA
-	array_word = [str(word) for word in array_word_int]
-	return array_word
+    # ALGORITMA
+    array_word = [str(word) for word in array_word_int]
+    return array_word
+
 
 def array_word_to_row(array_word):
-	# semicolon add
-	row = ";".join(array_word) + "\n"
-	return row
+    # semicolon add
+    row = ";".join(array_word) + "\n"
+    return row
+
 
 def array_to_csv(csv, folder_path, database):
-	save_csv = []
-	for array_word_int in database:
-		array_word = to_str(array_word_int)
-		row = ";".join(array_word) + "\n"
-		save_csv.append(row)
-	csv_path = os.path.join(folder_path,csv)
-	f = open(csv_path,"w")
-	for row in save_csv:
-		f.write(row)
-	f.close()
+    save_csv = []
+    for array_word_int in database:
+        array_word = to_str(array_word_int)
+        row = ";".join(array_word) + "\n"
+        save_csv.append(row)
+    csv_path = os.path.join(folder_path, csv)
+    f = open(csv_path, "w")
+    for row in save_csv:
+        f.write(row)
+    f.close()
+
 
 def save(databases):
-	folder_save = input("Masukkan nama folder penyimpanan: ")
-	# cek foldernya ada dan csvnya ada atau ngga
-	folder_path, workspace = cek_folder(folder_save)
-	# kalo gaada folder , buat folder baru
-	if folder_path == 'folder tidak ada':
-		folder_path = os.path.join(workspace, folder_save)
-		# buat folder, buat csv
-		os.makedirs(folder_path)
-	# kalo folder ada, overwrite
-	print("Saving...")
-	for i in range(len(databases)):
-		csv = nama_csv[i]
-		array_to_csv(csv, folder_path, databases[i])
-	print("Data telah disimpan pada folder", folder_save + "!")
-	return databases
+    folder_save = input("Masukkan nama folder penyimpanan: ")
+    # cek foldernya ada dan csvnya ada atau ngga
+    folder_path, workspace = cek_folder(folder_save)
+    # kalo gaada folder , buat folder baru
+    if folder_path == 'folder tidak ada':
+        folder_path = os.path.join(workspace, folder_save)
+        # buat folder, buat csv
+        os.makedirs(folder_path)
+    # kalo folder ada, overwrite
+    print("Saving...")
+    for i in range(len(databases)):
+        csv = nama_csv[i]
+        array_to_csv(csv, folder_path, databases[i])
+    print("Data telah disimpan pada folder", folder_save + "!")
+    return databases
+
 
 '''
 hasil array to csv
-['id;username;nama;alamat;password;role\n', 
-'1;morenzoe;Eraraya Morenzo Muten;Depok;kucing123;Admin\n', 
-'2;nisa,Khafifanisa;Bukittinggi;123sti;User\n', 
-'3;raihan;M. Raihan Aulia;Indonesia,kucing456,User\n', 
+['id;username;nama;alamat;password;role\n',
+'1;morenzoe;Eraraya Morenzo Muten;Depok;kucing123;Admin\n',
+'2;nisa,Khafifanisa;Bukittinggi;123sti;User\n',
+'3;raihan;M. Raihan Aulia;Indonesia,kucing456,User\n',
 '4;abik;Atabik Azfa;Rumah;inipass;User\n']
 
 goal
-['id;username;nama;alamat;password;role\n', 
-'1;morenzoe;Eraraya Morenzo Muten;Depok;kucing123 ;Admin\n', 
-'2;nisa,Khafifanisa;Bukittinggi;123sti;User\n', 
-'3;raihan;M. Raihan Aulia;Indonesia,kucing456,User\n', 
+['id;username;nama;alamat;password;role\n',
+'1;morenzoe;Eraraya Morenzo Muten;Depok;kucing123 ;Admin\n',
+'2;nisa,Khafifanisa;Bukittinggi;123sti;User\n',
+'3;raihan;M. Raihan Aulia;Indonesia,kucing456,User\n',
 '4;abik;Atabik Azfa; Rumah;inipass;User']
 '''
