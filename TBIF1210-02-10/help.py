81a5cde73ac6d6d55d04788dd03ffe0255328096
khafifanisa <82304706+khffnsa@users.noreply.github.com>
2021-05-01 00:01:54 +0700

    tambah keterangan.

--- TBIF1210-02-10/help.py
+++ TBIF1210-02-10/help.py
@@ -1,18 +1,25 @@
+"""
+Program F16 - Help
+Fungsi ini akan memberikan panduan penggunaan sistem.
+
+Akses : Admin, User
+"""
+
 def help(databases):
 	print("""=================================== H E L P ===================================
-register       : untuk melakukan registrasi user baru
-login          : untuk melakukan login ke dalam sistem
-carirariry     : untuk melakukan pencarian gadget dengan rarity tertentu
-caritahun      : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
-tambahitem     : untuk menambahkan item ke dalam inventori
-hapusitem      : untuk menghapus suatu item pada database
-ubahjumlah     : untuk mengubah jumlah gadget dan consumable dalam sistem
-pinjam         : untuk melakukan peminjaman gadget
-kembalikan     : untuk melakukan pengembalian gadget
-minta          : untuk meminta consumable yang tersedia
-riwayatpinjam  : untuk melihat riwayat peminjaman gadget
-riwayatkembali : untuk melihat riwayat pengembalian gadget
-riwayatambil   : untuk melihat riwayat pengembalian consumable
-exit           : untuk keluar dari aplikasi
+register      : untuk melakukan registrasi user baru
+login         : untuk melakukan login ke dalam sistem
+carirariry    : untuk melakukan pencarian gadget dengan rarity tertentu
+caritahun     : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
+tambahitem    : untuk menambahkan item ke dalam inventori
+hapusitem     : untuk menghapus suatu item pada database
+ubahjumlah    : untuk mengubah jumlah gadget dan consumable dalam sistem
+pinjam        : untuk melakukan peminjaman gadget
+kembalikan    : untuk melakukan pengembalian gadget
+minta         : untuk meminta consumable yang tersedia
+riwayatpinjam : untuk melihat riwayat peminjaman gadget
+riwayatkembali: untuk melihat riwayat pengembalian gadget
+riwayatambil  : untuk melihat riwayat pengembalian consumable
+exit          : untuk keluar dari aplikasi
 ===============================================================================""")
 	return databases
