900e3c30008cea088b9bc4e6a68cac7e0e62ea33
FlitzyBlue332 <raihanaulia332@gmail.com>
2021-04-30 22:04:19 +0700

    added validasi

--- TBIF1210-02-10/caritahun.py
+++ TBIF1210-02-10/caritahun.py
@@ -13,6 +13,29 @@ def printdata(data):
     print("Rarity          : ", data[4])
     print("Tahun Ditemukan : ", data[5])
 
+def ubahtahun(tahun_str):
+    # mengubah input tahun menjadi integer jika bisa, jika tidak bisa akan diminta kembali dari input user
+
+    # kamus
+    # algoritma
+    check = 0
+    while check == 0:
+        if len(tahun_str)==4:
+            for i in range(len(tahun_str)):
+                if tahun_str[i] in "0123456789":
+                    check = 1
+        else:
+            tahun_str = input("Masukan salah! Masukkan tahun dengan benar!: ")
+    return int(tahun_str)
+
+def ubahkategori(kategori):
+    # meminta input user untuk kategori jika input user salah
+
+    #kamus
+    #algoritma
+    while not(kategori == ">" or kategori == "<" or kategori == "=" or kategori == ">=" or kategori == "<="):
+        kategori = input("Masukan salah! Masukkan kategori dengan benar! : ")
+    return kategori
 def caritahun(database):
     # meng-output data gadget berdasarkan tahun ditemukannya
 
@@ -27,8 +50,10 @@ def caritahun(database):
     isLoggedIn = cek_active_account(database)
     if isLoggedIn:
         array_data = database[gadget]
-        tahun = int(input("Masukkan tahun    : "))
+        tahun_str = input("Masukkan tahun    : ")
+        tahun = ubahtahun(tahun_str)
         kategori =  (input("Masukkan kategori : ")) # input user berupa ">" , "<" , "=" , "<=" , ">="
+        kategori = ubahkategori(kategori)
         print("\nHasil pencarian :")
         if (kategori == ">"):
             for data in range(1,len(array_data)):
