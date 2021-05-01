1eed53ee97e3cf80a8f0d89dc6f725812c174a80
Eraraya Morenzo <morenzomuten@gmail.com>
2021-05-01 17:49:44 +0700

    Added username calling.

--- TBIF1210-02-10/exit.py
+++ TBIF1210-02-10/exit.py
@@ -1,6 +1,6 @@
 """Program F17 - Exit
-Fungsi ini akan menawarkan pengguna untuk menyimpan data.  Jika 
-pengguna ingin menyimpan data, fungsi akan memanggil fungsi save.  
+Fungsi ini akan menawarkan pengguna untuk menyimpan data.  Jika
+pengguna ingin menyimpan data, fungsi akan memanggil fungsi save.
 Setelahnya, fungsi akan memberhentikan keseluruhan program.
 
 Akses : -
@@ -12,28 +12,40 @@ import sys
 
 # Daftar library lokal
 from save import save
+from constant import active_account
+
+# Daftar Konstanta
+# active_account : int
 
 # Daftar variabel
 # save_option : str
 
 # ALGORITMA
 def exit(databases):
+    # Mendapatkan data terkait pengguna
+    try:
+        username = databases[active_account][1]
+    except IndexError:
+        username = "Anda"
+
     # Memvalidasi input dari pengguna
     while True:
-        save_option = input("Apakah Anda mau melakukan penyimpanan file "
+        save_option = input("(^.^): Apakah "
+                            + username
+                            + " mau melakukan penyimpanan file "
                             + "yang sudah diubah? (Y/N) ")
-        
+
         # Input valid, salah satu huruf Y/y/N/n
-        if save_option in "YyNn" and len(save_option)==1:
+        if save_option in "YyNn" and len(save_option) == 1:
             break
-        
+
         # Input tidak valid, pengisian diulang
-        print("\nm(><)m : Input tidak sesuai. Ulangi! \n")
-    
+        print("\nm(>.<)m: Input tidak sesuai. Ulangi! \n")
+
     # Pengguna ingin menyimpan data, melakukan fungsi save
     if save_option in "Yy":
         print()
         save(databases)
-    
+
     # Program selesai, terminasi loop program utama
-    sys.exit("\n(^O^)/ : Sampai jumpa!")
+    sys.exit("\n(^O^)/: Sampai jumpa!")
