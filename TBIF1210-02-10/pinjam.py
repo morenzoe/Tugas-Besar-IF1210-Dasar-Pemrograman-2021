d2197c40ae6773321684b5718aa7be006c069d6d
Eraraya Morenzo <morenzomuten@gmail.com>
2021-05-03 06:42:12 +0700

    F08 - Meminjam Gadget

--- TBIF1210-02-10/pinjam.py
+++ TBIF1210-02-10/pinjam.py
@@ -1,7 +1,7 @@
 """Program F08 - Meminjam Gadget
 Fungsi ini akan meminta input id gadget yang ingin dipinjam,
 jumlah dan tanggal. Peminjaman berhasil jika semua input valid.
-Setelah melakukan peminjaman, jumlah id tersebut pada file gadget 
+Setelah melakukan peminjaman, jumlah id tersebut pada file gadget
 akan diubah. Lalu, dibuat entri baru pada file riwayat pinjam.
 
 Akses : User
@@ -22,22 +22,22 @@ from login import cek_active_account
 # history	    : array of array
 # isLoggedIn	: bool
 # type          : str
-# id            : str		
+# id            : str
 # name          : str
 # id_peminjam	: str
-# idx           : int				
+# idx           : int
 # id_pinjam     : int
 # stock			: int
 # user          : array
-# new_history	: array 
+# new_history	: array
 
-# Definisi, Spesifikasi, dan Realisasi Fungsi/Prosedur  
+# Definisi, Spesifikasi, dan Realisasi Fungsi/Prosedur
 def cek_jumlah(id, type, jumlah, databases):
-    """Fungsi ini akan menghasilkan True 
-    apabila jumlah id <= stok id 
+    """Fungsi ini akan menghasilkan True
+    apabila jumlah id <= stok id
     pada databases[type].
     """
-    
+
     # KAMUS LOKAL
     # db  : array of array
     # idx : int
@@ -51,33 +51,33 @@ def cek_jumlah(id, type, jumlah, databases):
         else:
             return False
 
-  
+
 def input_tanggal(transaksi):
-    """Fungsi ini akan menghasilkan 
-    tanggal transkasi sesuai format 
+    """Fungsi ini akan menghasilkan
+    tanggal transkasi sesuai format
     dd/mm/yyyy menggunakan fungsi datetime.
     """
-    
+
     # KAMUS LOKAL
     # date_input : str
 
     # ALGORITMA
-    date_input = input("Tanggal "+transaksi+" : ")
+    date_input = input("Tanggal " + transaksi + " : ")
     try:
-        tanggal = dtm.strptime(date_input , '%d/%m/%Y' )
+        tanggal = dtm.strptime(date_input, '%d/%m/%Y')
         return date_input
-    except:
+    except BaseException:
         return 0
- 
- 
+
+
 def input_jumlah(id, databases, type):
-    """Fungsi ini akan menghasilkan 
-    jumlah yang valid dari id sesuai 
-    databases[type]. Jumlah valid, 
+    """Fungsi ini akan menghasilkan
+    jumlah yang valid dari id sesuai
+    databases[type]. Jumlah valid,
     jumlah > 0, jumlah <= stok.
 
     """
-    
+
     # KAMUS LOKAL
 
     # ALGORITMA
@@ -85,7 +85,7 @@ def input_jumlah(id, databases, type):
         count_input = input("Jumlah               : ")
         try:
             count = int(count_input)
-        except:
+        except BaseException:
             return 0
         if count > 0:
             if cek_jumlah(id, type, count, databases):
@@ -95,18 +95,22 @@ def input_jumlah(id, databases, type):
         else:
             print("Jumlah harus lebih besar dari 0!")
     return count
-    
-    
-def cek_borrow_history(gadget_id, databases): #mengecek apakah user pernah meminjam gadget yg sama, menghasilkan true jika sudah pernah meminjam
+
+
+# mengecek apakah user pernah meminjam gadget yg sama, menghasilkan true
+# jika sudah pernah meminjam
+def cek_borrow_history(gadget_id, databases):
     id = databases[active_account][0]
     db = databases[gadget_borrow_history]
     if len(db) > 1:
         for row in range(len(db)):
-            if int(id) == db[row][1] and gadget_id == db[row][2] and db[row][5] == "False":
+            if int(
+                    id) == db[row][1] and gadget_id == db[row][2] and db[row][5] == "False":
                 return True
     return False
-    
-# ALGORITMA PROGRAM UTAMA    
+
+
+# ALGORITMA PROGRAM UTAMA
 def pinjam(databases):
     isLoggedIn = cek_active_account(databases)
     type = "gadget"
@@ -138,23 +142,27 @@ def pinjam(databases):
                                 if tanggal != 0:
                                     jumlah = input_jumlah(id, databases, type)
                                     if jumlah != 0:
-                                        # Proses rewrite gadget                                   
+                                        # Proses rewrite gadget
                                         stock = stock - jumlah
                                         data[idx][3] = stock
-                                        
+
                                         # proses write gadget_borrow_history
                                         id_pinjam = len(history)
                                         id_peminjam = user[0]
                                         is_returned = "False"
-                                        new_history = [str(id_pinjam), int(id_peminjam), id, tanggal, jumlah, is_returned]
+                                        new_history = [
+                                            str(id_pinjam), int(id_peminjam), id, tanggal, jumlah, is_returned]
                                         history.append(new_history)
-                                        
+
                                         # Output
-                                        print("\n\(^ω^)/ : Item", name, "("+str(jumlah)+") telah berhasil dipinjam!")
+                                        print(
+                                            "\n\\(^ω^)/ : Item", name, "(" + str(jumlah) + ") telah berhasil dipinjam!")
                                     else:
-                                        print("\n┐(´д`)┌ : Maaf, input jumlah tidak valid!")
+                                        print(
+                                            "\n┐(´д`)┌ : Maaf, input jumlah tidak valid!")
                                 else:
-                                    print("\n┐(´д`)┌ : Maaf, input tanggal tidak sesuai format dd/mm/yyyy!")
+                                    print(
+                                        "\n┐(´д`)┌ : Maaf, input tanggal tidak sesuai format dd/mm/yyyy!")
                             else:
                                 print("\n(>_<) : Maaf, persediaan habis!")
                     else:
@@ -163,5 +171,5 @@ def pinjam(databases):
                     print("\n┐(´д`)┌ : Maaf, input id tidak valid!")
     else:
         print("(^_^) : Silahkan login terlebih dahulu!")
-    
+
     return databases
