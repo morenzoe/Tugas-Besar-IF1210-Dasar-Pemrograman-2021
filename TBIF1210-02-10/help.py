d30e1c29b90c3d9723be6fccda6011a993d00add
khafifanisa <82304706+khffnsa@users.noreply.github.com>
2021-05-02 16:38:20 +0700

    update.

--- TBIF1210-02-10/help.py
+++ TBIF1210-02-10/help.py
@@ -15,21 +15,21 @@ def help(databases):
     if not isLoggedIn:
         print(
             """=================================== H E L P ===================================
-register      : untuk melakukan registrasi user baru
-login         : untuk melakukan login ke dalam sistem
-carirariry    : untuk melakukan pencarian gadget dengan rarity tertentu
-caritahun     : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
-tambahitem    : untuk menambahkan item ke dalam inventori
-hapusitem     : untuk menghapus suatu item pada database
-ubahjumlah    : untuk mengubah jumlah gadget dan consumable dalam sistem
-pinjam        : untuk melakukan peminjaman gadget
-kembalikan    : untuk melakukan pengembalian gadget
-minta         : untuk meminta consumable yang tersedia
-riwayatpinjam : untuk melihat riwayat peminjaman gadget
-riwayatkembali: untuk melihat riwayat pengembalian gadget
-riwayatambil  : untuk melihat riwayat pengembalian consumable
-save          : untuk menyimpan data ke dalam file
-exit          : untuk keluar dari aplikasi
+register       : untuk melakukan registrasi user baru
+login          : untuk melakukan login ke dalam sistem
+carirariry     : untuk melakukan pencarian gadget dengan rarity tertentu
+caritahun      : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
+tambahitem     : untuk menambahkan item ke dalam inventori
+hapusitem      : untuk menghapus suatu item pada database
+ubahjumlah     : untuk mengubah jumlah gadget dan consumable dalam sistem
+pinjam         : untuk melakukan peminjaman gadget
+kembalikan     : untuk melakukan pengembalian gadget
+minta          : untuk meminta consumable yang tersedia
+riwayatpinjam  : untuk melihat riwayat peminjaman gadget
+riwayatkembali : untuk melihat riwayat pengembalian gadget
+riwayatambil   : untuk melihat riwayat pengembalian consumable
+save           : untuk menyimpan data ke dalam file
+exit           : untuk keluar dari aplikasi
 ===============================================================================""")
 
     else:
@@ -37,28 +37,28 @@ exit          : untuk keluar dari aplikasi
         if role == "Admin":
             print(
                 """=================================== H E L P ===================================
-register      : untuk melakukan registrasi user baru
-carirariry    : untuk melakukan pencarian gadget dengan rarity tertentu
-caritahun     : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
-tambahitem    : untuk menambahkan item ke dalam inventori
-hapusitem     : untuk menghapus suatu item pada database
-ubahjumlah    : untuk mengubah jumlah gadget dan consumable dalam sistem
-riwayatpinjam : untuk melihat riwayat peminjaman gadget
-riwayatkembali: untuk melihat riwayat pengembalian gadget
-riwayatambil  : untuk melihat riwayat pengembalian consumable
-save          : untuk menyimpan data ke dalam file
-exit          : untuk keluar dari aplikasi
+register       : untuk melakukan registrasi user baru
+carirariry     : untuk melakukan pencarian gadget dengan rarity tertentu
+caritahun      : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
+tambahitem     : untuk menambahkan item ke dalam inventori
+hapusitem      : untuk menghapus suatu item pada database
+ubahjumlah     : untuk mengubah jumlah gadget dan consumable dalam sistem
+riwayatpinjam  : untuk melihat riwayat peminjaman gadget
+riwayatkembali : untuk melihat riwayat pengembalian gadget
+riwayatambil   : untuk melihat riwayat pengembalian consumable
+save           : untuk menyimpan data ke dalam file
+exit           : untuk keluar dari aplikasi
 ===============================================================================""")
         else:
             print(
                 """=================================== H E L P ===================================
-carirariry    : untuk melakukan pencarian gadget dengan rarity tertentu
-caritahun     : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
-pinjam        : untuk melakukan peminjaman gadget
-kembalikan    : untuk melakukan pengembalian gadget
-minta         : untuk meminta consumable yang tersedia
-save          : untuk menyimpan data ke dalam file
-exit          : untuk keluar dari aplikasi
+carirariry : untuk melakukan pencarian gadget dengan rarity tertentu
+caritahun  : untuk melakukan pencarian gadget berdasarkan tahun ditemukan
+pinjam     : untuk melakukan peminjaman gadget
+kembalikan : untuk melakukan pengembalian gadget
+minta      : untuk meminta consumable yang tersedia
+save       : untuk menyimpan data ke dalam file
+exit       : untuk keluar dari aplikasi
 ===============================================================================""") 
 
     return databases            
