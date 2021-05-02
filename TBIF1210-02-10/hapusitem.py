f52596c9a4ffb2d8c2d475f56fc4de74e7b4afc1
atabiksf <atabik.shofi@gmail.com>
2021-05-02 13:17:55 +0700

    Coba dicek ini baru kodenya, belum dirapihin dsb.

--- TBIF1210-02-10/hapusitem.py
+++ TBIF1210-02-10/hapusitem.py
@@ -1,4 +1,4 @@
-from constant import gadget,consumable,active_account
+from constant import gadget, consumable, active_account, gadget_borrow_history
 from login import cek_active_account
 
 def cek_role(data): #True jika admin, False jika user
@@ -8,6 +8,7 @@ def cek_role(data): #True jika admin, False jika user
     else:
         return False
 
+
 def db_cek(data, type):
     db_gadget = data[gadget]
     db_consumable = data[consumable]
@@ -16,12 +17,18 @@ def db_cek(data, type):
     elif type == "consumable":
         return db_consumable
 
+
 def cek_id(id, data, type):
     db = db_cek(data, type)
+    cek = 0
     for row in range(len(db)):
         if id == db[row][0]:
-            return True
-    return False
+            cek = 1
+    if cek == 0:
+        return False
+    elif cek == 1:
+        return True
+    
     
 def cek_idx(id, data, type):
     db = db_cek(data, type)
@@ -29,41 +36,59 @@ def cek_idx(id, data, type):
         if id == db[row][0]:
             idx = row
     return idx
+    
 
+def cek_user_borrow_history(gadget_id, data): #mengecek apakah gadget sedang dipinjam oleh user , menghasilkan true jika sedang dipinjam
+    f = data[gadget_borrow_history]
+    for row in range(len(f)):
+        if gadget_id == f[row][2] and f[row][5] == "False":
+            return True
+    return False
+
+    
 def hapusitem(databases):
     isLoggedin = cek_active_account(databases)
     
     if isLoggedin:        
         if cek_role(databases):
             id = input("Masukan ID item: ")
-            if id[0] == "G":
-                type = "gadget"
-                data = databases[gadget]
-            elif id[0] == "C":
-                type = "consumable"
-                data = databases[consumable]
-            else:
-                print("Tidak ada item dengan ID tersebut.")
+            if len(id) == 0 :
+                print("\n┐(´д`)┌ : Maaf, input id tidak valid!")
                 return databases
+            else:
+                if id[0] == "G":
+                    type = "gadget"
+                    data = databases[gadget]
+                elif id[0] == "C":
+                    type = "consumable"
+                    data = databases[consumable]
+                else:
+                    print("\n┐(´д`)┌ : Maaf, input id tidak valid!")
+                    return databases
             
             if cek_id(id, databases, type):
-               idx = cek_idx(id, databases, type)
-               name = data[idx][1]
-               while True:
-                   validation = input("Apakah anda yakin ingin menghapus "+name+" (Y/N)? ")
-                   if validation in "YyNn" and len(validation) == 1 :
-                       break
-               if validation in "Yy":
-                  #proses
-                  del data[idx]
-                  print("Item telah berhasil dihapus dari database")
-               else:
-                   print("Penghapusan item dibatalkan")
+                if type == "gadget" and cek_user_borrow_history(id, databases):
+                    print("\n┐(´д`)┌ : Maaf, item sedang dipinjam oleh user. Tidak dapat menghapus item!")
+                else:
+                    idx = cek_idx(id, databases, type)
+                    name = data[idx][1]
+                    while True:
+                        validation = input("\n(/_\) : Apakah anda yakin ingin menghapus "+name+" yang berharga ini? (Y/N) ")
+                        if validation in "YyNn" and len(validation) == 1 :
+                            break
+                        else:
+                            print("(^_^) : Maaf, silahkan masukan Y/y untuk yes dan N/n untuk no!")
+                    if validation in "Yy":
+                        #proses
+                        del data[idx]
+                        print("\n(>_<) : Item telah dihapus dari databases!")
+                    else:
+                        print("\n\(^ω^)/ : Penghapusan item dibatalkan!")
             else:
-                print("Tidak ada item dengan ID tersebut.")
+                print("\n┐(´д`)┌ : Maaf, tidak ada item dengan ID tersebut!")
         else:
-            print("Maaf, perintah ini hanya dapat diakses oleh Admin")
+            print("(^_^) : Maaf, perintah ini hanya dapat diakses oleh Admin!")
     else:
-        print("Silahkan login terlebih dahulu")
+        print("(^_^) : Silahkan login terlebih dahulu!")
     
     return databases
--- TBIF1210-02-10/kembalikan.py
+++ TBIF1210-02-10/kembalikan.py
@@ -5,19 +5,25 @@ from minta import input_tanggal
 
 def find_gadget_name(id_peminjaman, data):
     f = data[gadget]
-    id_gadget = data[gadget_borrow_history][id_peminjaman-1][2]
+    id_gadget = data[gadget_borrow_history][id_peminjaman][2]
     for row in range(len(f)):
         if id_gadget == f[row][0]:
             name = f[row][1]
     return name
 
-def cek_riwayat_peminjaman(data): #Mengecek apakah user pernah meminjam gadget atau tidak
+
+def cek_riwayat_peminjaman(data): #Mengecek apakah user pernah meminjam gadget atau tidak, true jika sudah, false jika belum
     f = data[gadget_borrow_history]
     id = data[active_account][0]
+    cek = 0
     for row in range(len(f)):
-        if int(id) == f[row][1] and f[row][5] == False:
-            return True
-    return False
+        if int(id) == f[row][1] and f[row][5] == "False":
+            cek = 1
+    if cek == 0:
+        return False
+    elif cek == 1:
+        return True
+
 
 def kembalikan(databases):
     isLoggedin = cek_active_account(databases)
@@ -30,50 +36,60 @@ def kembalikan(databases):
         user = databases[active_account]
         id = user[0]
         if cek_role(databases):
-            print("Maaf, perintah ini hanya dapat diakses oleh user")
+            print("(^_^) : Maaf, perintah ini hanya dapat diakses oleh user!")
         else:
             # Menampilkan riwayat peminjaman user
             num = 0
             if cek_riwayat_peminjaman(databases):
                 for row in range(len(data)):
-                    if int(id) == data[row][1] and data[row][5] == False:
+                    if int(id) == data[row][1] and data[row][5] == "False":
                         num = num + 1
-                        name = find_gadget_name(row+1, databases)
+                        name = find_gadget_name(row, databases)
                         print(str(num)+".", name)
                 print()
                 # Input no peminjaman
-                number = int(input("Masukan nomor peminjaman: "))
+                try:
+                    number = int(input("Masukan nomor        : "))
+                except:
+                    print("\n┐(´д`)┌ : Maaf, input tidak valid!")
+                    return databases
                 if number < 0 or number > num :
-                    print("Nomor peminjaman di luar pilihan!")
+                    print("\n┐(´д`)┌ : Maaf, nomor peminjaman di luar pilihan!")
                 else:
                     idx_found = 0
+                    row = 0
                     while row <= (len(data)):
-                        if int(id) == data[row][1] and data[row][5] == False:
+                        if int(id) == data[row][1] and data[row][5] == "False":
                             idx_found = idx_found + 1
-                            if idx_found == number:
-                                break
+                        if idx_found == number:
+                            idx_row = row
+                            break
+                        row = row + 1
                     # Input tanggal
                     tanggal = input_tanggal("pengembalian")
-                    # Proses
-                    borrow_history = data[row]
-                    id_peminjaman = borrow_history[0] # mencari id peminjaman
-                    id_gadget = borrow_history[2] # mencari id gadget
-                    jumlah = borrow_history[4] # mencari jumlah peminjaman
-                    idx = cek_idx(id_gadget, databases, type) # mencari idx gadget di db_gadget
-                    borrowed_gadget = db_gadget[idx]
-                    gadget_name = borrowed_gadget[1]
-                    # Prsoses rewrite db_gadget
-                    borrowed_gadget[3] = borrowed_gadget[3] + jumlah
-                    # Proses rewrite borrow_history
-                    borrow_history[5] = True
-                    # Proses append return_history
-                    id_return = len(history)
-                    new_data = [str(id_return), int(id_peminjaman), tanggal]
-                    history.append(new_data)
-                    #output
-                    print("Item", gadget_name, "("+str(jumlah)+") telah dikembalikan")
+                    if tanggal != 0:
+                        # Proses
+                        borrow_history = data[idx_row]
+                        id_peminjaman = borrow_history[0] # mencari id peminjaman
+                        id_gadget = borrow_history[2] # mencari id gadget
+                        jumlah = borrow_history[4] # mencari jumlah peminjaman
+                        idx = cek_idx(id_gadget, databases, type) # mencari idx gadget di db_gadget
+                        borrowed_gadget = db_gadget[idx]
+                        gadget_name = borrowed_gadget[1]
+                        # Prsoses rewrite db_gadget
+                        borrowed_gadget[3] = borrowed_gadget[3] + jumlah
+                        # Proses rewrite borrow_history
+                        borrow_history[5] = "True"
+                        # Proses append return_history
+                        id_return = len(history)
+                        new_data = [str(id_return), int(id_peminjaman), tanggal]
+                        history.append(new_data)
+                        #output
+                        print("\n\(^ω^)/ : Item", gadget_name, "("+str(jumlah)+") telah dikembalikan!")
+                    else:
+                        print("\n┐(´д`)┌ : Maaf, input tanggal tidak sesuai format dd/mm/yyyy!")
             else:
-                print("Anda tidak meminjam gadget apapun!")
+                print("\n(^o^) : Anda sedang tidak meminjam gadget apapun!")
     else:
-        print("Silahkan login terlebih dahulu")
+        print("(^_^) : Silahkan login terlebih dahulu!")
     return databases
